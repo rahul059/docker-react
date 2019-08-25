fffa77710866c69affdb6170dc9a9d779d7b1b82
rahul singh <rs28851@gmail.com>
2019-08-26 01:12:34 +0530

    chnge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Duniya madarchod h.
+          Duniya madarchod hai.
         </p>
       </header>
     </div>
